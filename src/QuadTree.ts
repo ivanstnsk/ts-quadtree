1f434b4d75efeef1b52b56216951e5b1d8529deb
ivanstnsk <ivan.stnsk@gmail.com>
2020-03-02 13:27:49 +0200

    Updated split method

--- src/QuadTree.ts
+++ src/QuadTree.ts
@@ -2,7 +2,7 @@ import Rectangle from './Rectangle';
 
 export default class QuadTree {
   readonly bounds: Rectangle;
-  readonly nodes: QuadTree[];
+  readonly nodes: QuadTree[]; // [tl, tr, bl, br]
 
   constructor(bounds: Rectangle) {
     this.bounds = bounds;
@@ -13,34 +13,15 @@ export default class QuadTree {
     const subWidth = this.bounds.width / 2;
     const subHeight = this.bounds.height / 2;
 
-    const topLeftBounds = {
-      x: this.bounds.x,
-      y: this.bounds.y,
-      width: subWidth,
-      height: subHeight
-    };
-    const topRightBounds = {
-      x: this.bounds.x + subWidth,
-      y: this.bounds.y,
-      width: subWidth,
-      height: subHeight,
-    };
-    const bottomLeftBounds = {
-      x: this.bounds.x,
-      y: this.bounds.y + subHeight,
-      width: subWidth,
-      height: subHeight
-    };
-    const bottomRightBounds = {
-      x: this.bounds.x + subWidth,
-      y: this.bounds.y + subHeight,
-      width: subWidth,
-      height: subHeight,
-    };
-
-    this.nodes.push(new QuadTree(topLeftBounds));
-    this.nodes.push(new QuadTree(topRightBounds));
-    this.nodes.push(new QuadTree(bottomLeftBounds));
-    this.nodes.push(new QuadTree(bottomRightBounds));
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        this.nodes.push(new QuadTree({
+          x: this.bounds.x + subWidth * i,
+          y: this.bounds.y + subHeight * j,
+          width: subWidth,
+          height: subHeight,
+        }));
+      }  
+    }
   }
 }
